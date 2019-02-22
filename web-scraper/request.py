6a072da1b60bd97b193e5496f00638781290e483
zhaoxiang <zhaoxiang@blogdaily.net>
2019-02-22 14:32:30 +0000

    - add getStations() and getContract() - remove some comments

--- web-scraper/request.py
+++ web-scraper/request.py
@@ -5,18 +5,21 @@ This file contains only one function, the function return a json data read from 
 """ 
  @hostname: the host address of JCD
  @api_key: the api key
- @req: the full request which will be send from the client to JCD
 """
 
 hostname = 'https://api.jcdecaux.com/vls/v1/'
 api_key = '92be62130be380ad2925b05fc3b1e5b3ef1a5a55'
-req = hostname + 'stations' + '?' + 'apiKey=' + api_key
-"""
-Fetch the data from JCD, and return the json data 
-@Input: void
-@Output: the json data
- """
 
 
-def getData():
-    return requests.get(req)
+def getStations():
+    req = hostname + 'stations' + '?apiKey=' + api_key
+    data = requests.get(req).json
+
+    return data
+
+
+def getContract():
+    req = hostname + 'contracts' + "?apiKey=" + api_key
+    data = requests.get(req).json
+
+    return data
