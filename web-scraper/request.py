d97252cdb8bde745aa7c7784bf8dc9d15c0904cb
zhaoxiang <zhaoxiang@blogdaily.net>
2019-02-22 14:38:19 +0000

    rename function to get_stations() and get_contracts()

--- web-scraper/request.py
+++ web-scraper/request.py
@@ -11,15 +11,15 @@ hostname = 'https://api.jcdecaux.com/vls/v1/'
 api_key = '92be62130be380ad2925b05fc3b1e5b3ef1a5a55'
 
 
-def getStations():
+def get_stations():
     req = hostname + 'stations' + '?apiKey=' + api_key
-    data = requests.get(req).json
+    data = requests.get(req).json()
 
     return data
 
 
-def getContract():
+def get_contracts():
     req = hostname + 'contracts' + "?apiKey=" + api_key
-    data = requests.get(req).json
+    data = requests.get(req).json()
 
     return data
