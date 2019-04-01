cc421eac9f37296d4d13fc8420a995dce3e685b1
Scarlet Grant <scarlet.grant@ucdconnect.ie>
2019-04-01 21:57:43 +0100

    Fixed render_template

--- app.py
+++ app.py
@@ -1,38 +1,67 @@
 '''Code is commented for beginning programmers to read and understand every line.
     Will be simplified afterwards.
 '''
+#!/usr/bin/env/python
 # import Flask in this app script from flask on the server
 # import render_template to render a template from the app, in this case our index.html that we saved as template. Don't forget the comma
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
-# app is a Flask application/function of Flask with the name and filename of the app
-app = Flask(__name__)
+# Creat a Flask application with the name and filename of the app. Static files are served from the 'statis' directory.
+app = Flask(__name__)#, static_url_path='')
 
-# the route specifies where the location of the file? and url.
+# the route specifies the internal url and serves 'index.html'.
 @app.route("/")
-# eventually create extra route to the same view
-@app.route("/home")
-
-# function to return index.html. We create a folder calld templates in the root forlder as well where we store the index.html
 def index():
-    # it renders our index.html now. However, not the CSS that I also stored in templates.
     return render_template('index.html')
-#creates extra page view
-"""
-#check tutorial 2 on minute 11, how to dynamically change title etc. https://www.youtube.com/watch?v=QnDWIZuWYW0
-@app.route("/about")
-def about():
-    return "<h1>About</h1>"
-"""
-# It won't make sense to load all elements again and again with each view so we create a layout.html to store all of this and then wrap the current view in to this
-# do all this in layout.html and put the body in {% block body %} {% end block %}
+
+@app.route('/map')
+def map():
+    return "<h1>map</h1>"
+
+
+
+
+#from flask import abort
+#app.route("/user/<id>")
+#def get_user(id):
+#    user = load_user(id) if not user:
+#    abort(404)
+    #return '<h1>Hello, %s</h1>' % user.name
+
+
 
 # Ads condition in the script and runs it in debug mode
-# If the script is the main function than run it. Check that it is this script that is actually being executed and not any other.
+# If the script is the main function than run it. Check that it is this script that is 
+# actually being executed and not any other.
 if __name__ == "__main__":
     # if yes, than run (when you put the app in debug mode you don't have to restart the server all the time to see any changes in the script.)
     # app.debug = True
     app.run(debug=True)
 
-    # flask wtform
-# forms wysiwyg form editor
+# It won't make sense to load all elements again and again with each view so we create a 
+# layout.html to store all of this and then wrap the current view in to this
+# do all this in layout.html and put the body in {% block body %} {% end block %}
+
+#@app.route("/bike-stations")
+#def get_bike-stations():
+    # returns all bike stations
+    # function
+    #return #
+
+#@app.route("/available-bikes")
+#def available-bikes():
+    # returns number of available bikes at stations within 250/500/100 mt
+    # function
+    #return #
+    
+#@app.route("/weather-forecast")
+#def weather-forecast():
+    # returns forecasted weather 
+    # function
+    #return #
+
+#@app.route("/available-bikes-forecast")
+#def available-bikes-forecast():
+    # returns forecast of available bikes
+    # function
+    #return #
