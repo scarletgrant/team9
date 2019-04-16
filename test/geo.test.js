3969abc3fa90c5c8fb27bb1c88611779b478d6ae
zhaoxiang <jiaruiqin@blogdaily.net>
2019-04-16 10:35:42 +0100

    Remove unused files

--- test/geo.test.js
+++
@@ -1,9 +0,0 @@
-const { describe, it } = require('mocha')
-const expect = require('chai').expect
-const geo = require('../lib/geo')
-
-describe('Geo Location OK Testing', () => {
-  it('should get the correct coordinates information', () => {
-    geo.coordinate('dublin', 'ie').catch(err => expect(err).to.null)
-  })
-})
